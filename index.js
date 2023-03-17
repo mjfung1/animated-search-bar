2c7fd3ab46957ead263dd0cb0565a71b1015d11f
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2023-03-17 17:36:45 -0400

    deploy

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 
 
 const searchBar = document.querySelector(".search-bar-container");
-const magnifier = document.querySelector(".magnifier")
+const magnifier = document.querySelector(".magnifier");
+const mic = document.querySelector(".mic-icon");
 
 magnifier.addEventListener("click", () => {
     searchBar.classList.toggle("active");
+});
+
+mic.addEventListener("click", () => {
+    searchBar.classList.add("active");
+});
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
 })
