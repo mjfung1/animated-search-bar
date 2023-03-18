267204b6fb3a466fa1bb6cbc55452cac3fb554f5
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2023-03-18 03:04:31 -0400

    add assistant and search functionality

--- index.js
+++ index.js
@@ -4,15 +4,26 @@
 const searchBar = document.querySelector(".search-bar-container");
 const magnifier = document.querySelector(".magnifier");
 const mic = document.querySelector(".mic-icon");
+const input = document.querySelector(".input");
+
+const audio = new Audio("assistant.mp3");
 
 magnifier.addEventListener("click", () => {
     searchBar.classList.toggle("active");
 });
 
 mic.addEventListener("click", () => {
-    searchBar.classList.add("active");
+    audio.play();
 });
 
+
+input.addEventListener("keypress", (e) => {
+
+    if (e.keyCode === 13) {
+        location.href = 'https://www.google.com/search?q=' + input.value;
+
+    }
+})
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
